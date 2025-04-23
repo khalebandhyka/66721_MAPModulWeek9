1db60cb3bb9dc830172929d5de59f80438514d60
Khaleb Andhyka Aprijadi <khaleb.andhyka@gmail.com>
2025-04-23 19:29:12 +0700

    Bagian 1, Checkpoint 1

--- app/src/main/java/com/alexwawo/w08firebase101/Student.kt
+++ app/src/main/java/com/alexwawo/w08firebase101/Student.kt
@@ -3,5 +3,6 @@ package com.alexwawo.w08firebase101
 data class Student(
     val id: String = "",
     val name: String = "",
-    val program: String = ""
+    val program: String = "",
+    val docId: String = ""
 )
