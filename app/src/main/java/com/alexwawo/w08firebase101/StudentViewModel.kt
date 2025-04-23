c3c7d5fe4921cf43d47df01e4ba2a42efcbe088d
Khaleb Andhyka Aprijadi <khaleb.andhyka@gmail.com>
2025-04-23 20:54:55 +0700

    Bagian 2, Checkpoint 2

--- app/src/main/java/com/alexwawo/w08firebase101/StudentViewModel.kt
+++ app/src/main/java/com/alexwawo/w08firebase101/StudentViewModel.kt
@@ -38,6 +38,38 @@ class StudentViewModel : ViewModel() {
             }
     }
 
+    fun updateStudent(student: Student) {
+        val updatedData = hashMapOf(
+            "id" to student.id,
+            "name" to student.name,
+            "program" to student.program,
+            "phones" to student.phones
+        )
+        db.collection("students")
+            .document(student.docId)
+            .set(updatedData)
+            .addOnSuccessListener {
+                Log.d("Firestore", "DocumentSnapshot updated with ID: ${student.docId}")
+                fetchStudents()
+            }
+            .addOnFailureListener { e ->
+                Log.w("Firestore", "Error updating document", e)
+            }
+    }
+    fun deleteStudent(student: Student) {
+        db.collection("students")
+            .document(student.docId)
+            .delete()
+            .addOnSuccessListener {
+                Log.d("Firestore", "DocumentSnapshot deleted with ID: ${student.docId}")
+                fetchStudents()
+            }
+            .addOnFailureListener { e ->
+                Log.w("Firestore", "Error deleting document", e)
+            }
+    }
+
+
     private fun fetchStudents() {
         db.collection("students")
             .get()
@@ -48,7 +80,8 @@ class StudentViewModel : ViewModel() {
                     val name = document.getString("name") ?: ""
                     val program = document.getString("program") ?: ""
                     val phones = document.get("phones") as? List<String> ?: emptyList()
-                    list.add(Student(id, name, program, phones))
+                    val docId = document.id
+                    list.add(Student(id, name, program, docId, phones))
                 }
                 students = list
             }
